75fa0d20cd9f847321c0f3e735c9c29ee3e142d2
Rodrigo Flores <floresrodrigodf@gmail.com>
2025-05-19 14:40:52 -0600

    task manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+
+  get '/tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post '/tasks/create', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: 'show_task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update_task'
+  delete 'tasks/:id', to: 'task#destroy', as: 'delete_task'
 
   # Defines the root path route ("/")
-  # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Task.create(title: "Laundry", details: "Do not mix colors!")
+Task.create(title: "Studying", details: "A lot of flashcards to do", completed: true)
